ef58ab243d2816cc989b279dbface3f881ede21b
Mateus Nadal <nadalmateus@hotmail.com>
2023-05-21 16:41:53 -0300

    add validation class

---
+++ src/Store.Core/DomainObjects/Validations.cs
@@ -0,0 +1,155 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Store.Core.DomainObjects
+{
+    public class Validations
+    {
+        public static void ValidateIfEqual(object object1, object object2, string message)
+        {
+            if (object1.Equals(object2))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfDifferent(object object1, object object2, string message)
+        {
+            if (!object1.Equals(object2))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfDifferent(string pattern, string value, string message)
+        {
+            var regex = new Regex(pattern);
+
+            if (!regex.IsMatch(value))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateLength(string value, int max, string message)
+        {
+            var length = value.Trim().Length;
+            if (length > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateLength(string value, int min, int max, string message)
+        {
+            var length = value.Trim().Length;
+            if (length < min || length > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfEmpty(string value, string message)
+        {
+            if (value == null || value.Trim().Length == 0)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfNull(object object1, string message)
+        {
+            if (object1 == null)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateRange(double value, double min, double max, string message)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateRange(float value, float min, float max, string message)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateRange(int value, int min, int max, string message)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateRange(long value, long min, long max, string message)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateRange(decimal value, decimal min, decimal max, string message)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfLessThan(long value, long min, string message)
+        {
+            if (value < min)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfLessThan(double value, double min, string message)
+        {
+            if (value < min)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfLessThan(decimal value, decimal min, string message)
+        {
+            if (value < min)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfLessThan(int value, int min, string message)
+        {
+            if (value < min)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfFalse(bool value, string message)
+        {
+            if (!value)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfTrue(bool boolvalue, string mensagem)
+        {
+            if (boolvalue)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+    }
+}
