20496f8b7088f66e64e3a55ccfac2f33a99f4126
Mateus Nadal <nadalmateus@hotmail.com>
2023-05-21 15:45:05 -0300

    add core entity class

---
+++ src/Store.Catalog.Domain/Product.cs
@@ -0,0 +1,12 @@
+﻿using Store.Core.DomainObjects;
+
+namespace Store.Catalog.Domain
+{
+    public class Product : Entity
+    {
+    }
+
+    public class Category
+    {
+    }
+}
---
+++ src/Store.Core/DomainObjects/Entity.cs
@@ -0,0 +1,39 @@
+﻿namespace Store.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var compareTo = obj as Entity;
+
+            return ReferenceEquals(this, compareTo) || (compareTo is not null && Id.Equals(compareTo.Id));
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+
+        public static bool operator !=(Entity? a, Entity? b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator ==(Entity? a, Entity? b)
+        {
+            return a is null && b is null || a is not null && b is not null && a.Equals(b);
+        }
+    }
+}
