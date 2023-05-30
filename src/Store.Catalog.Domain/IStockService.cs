45f2d4b331681505a03696aedf731d7af3cfe05e
Mateus Nadal <nadalmateus@hotmail.com>
2023-05-29 22:31:51 -0300

    add a stock domain service

---
+++ src/Store.Catalog.Domain/IStockService.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Catalog.Domain
+{
+    public interface IStockService
+    {
+        Task<bool> DebitStock(Guid productId, int quatity);
+
+        Task<bool> ReplenishStock(Guid productId, int quatity);
+    }
+}
---
+++ src/Store.Catalog.Domain/StockService.cs
@@ -0,0 +1,43 @@
+﻿namespace Store.Catalog.Domain
+{
+    public class StockService : IStockService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public StockService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> DebitStock(Guid productId, int quatity)
+        {
+            var product = await _productRepository.GetById(productId);
+
+            if (product == null) return false;
+
+            if (!product.HasStock(quatity)) return false;
+
+            product.DebitStock(quatity);
+
+            _productRepository.Update(product);
+            return await _productRepository.UnitOfWork.Commit();
+        }
+
+        public async Task<bool> ReplenishStock(Guid productId, int quatity)
+        {
+            var product = await _productRepository.GetById(productId);
+
+            if (product == null) return false;
+
+            product.ReplenishStock(quatity);
+
+            _productRepository.Update(product);
+            return await _productRepository.UnitOfWork.Commit();
+        }
+
+        public void Dispose()
+        {
+            _productRepository.Dispose();
+        }
+    }
+}
