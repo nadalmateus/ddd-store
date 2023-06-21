ad0efcb4cca4c67e7ca7a6f8d2cea8054cbac8da
Mateus Nadal <nadalmateus@hotmail.com>
2023-06-20 21:08:03 -0300

    add a mvc frontend. and migrations

---
+++ Store.WebApp.MVC/Setup/DependenceInjection.cs
@@ -0,0 +1,21 @@
+﻿using Store.Catalog.Application.Services;
+using Store.Catalog.Domain;
+using Store.Catalog.Domain.Data;
+using Store.Catalog.Domain.Data.Repository;
+using Store.Core.Bus;
+
+namespace Store.WebApp.MVC.Setup
+{
+    public static class DependenceInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductAppService, ProductAppService>();
+            services.AddScoped<IStockService, StockService>();
+            services.AddScoped<CatalogContext>();
+        }
+    }
+}
--- src/Store.Catalog.Domain.Data/Mappings/CategoryMapping.cs
+++ src/Store.Catalog.Domain.Data/Mappings/CategoryMapping.cs
@@ -8,11 +8,19 @@ public class CategoryMapping : IEntityTypeConfiguration<Category>
     public void Configure(EntityTypeBuilder<Category> builder)
     {
         builder.HasKey(c => c.Id);
-        builder.Property(c => c.Id).IsRequired().HasColumnType("varchar(250)");
 
-        builder.HasMany(c => c.Products).WithOne(c => c.Category).HasForeignKey(p => p.CategoryId)
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasColumnType("varchar(250)");
+
+        builder.Property(c => c.Code)
+           .IsRequired()
+           .HasColumnType("int");
+
+        builder.HasMany(c => c.Products)
+            .WithOne(p => p.Category)
             .HasForeignKey(p => p.CategoryId);
 
-        builder.ToTable("Categories");
+        builder.ToTable("Category");
     }
 }
