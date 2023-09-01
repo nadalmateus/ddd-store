e793d91e1c0df21083d8b2c8835cc95399810c75
Mateus Nadal <nadalmateus@hotmail.com>
2023-08-31 22:32:21 -0300

    add entities and validations

--- src/Store.Catalog.Domain/Category.cs
+++ src/Store.Catalog.Domain/Category.cs
@@ -5,5 +5,18 @@ namespace Store.Catalog.Domain
 {
     public class Category : Entity, IAggregateRoot
     {
+        public Category(string name, int code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public string Name { get; private set; }
+        public int Code { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Code}";
+        }
     }
 }
--- src/Store.Catalog.Domain/Product.cs
+++ src/Store.Catalog.Domain/Product.cs
@@ -5,24 +5,14 @@ namespace Store.Catalog.Domain
 {
     public class Product : Entity, IAggregateRoot
     {
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public bool Active { get; private set; }
-        public decimal Price { get; private set; }
-        public Guid CategoryId { get; private set; }
-        public DateTime RegistrationDate { get; private set; }
-        public string Image { get; private set; }
-        public int StockQuantity { get; private set; }
-        public Category Category { get; private set; }
-
         public Product(
-         string name,
-         string description,
-         bool active,
-         decimal price,
-         Guid categoryId,
-         DateTime registrationDate,
-         string image)
+        string name,
+        string description,
+        bool active,
+        decimal price,
+        Guid categoryId,
+        DateTime registrationDate,
+        string image)
         {
             Name = name;
             Description = description;
@@ -33,6 +23,16 @@ namespace Store.Catalog.Domain
             Image = image;
         }
 
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; }
+        public decimal Price { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public DateTime RegistrationDate { get; private set; }
+        public string Image { get; private set; }
+        public int StockQuantity { get; private set; }
+        public Category Category { get; private set; }
+
         public void Activate() => Active = true;
 
         public void Deactivate() => Active = false;
@@ -41,6 +41,31 @@ namespace Store.Catalog.Domain
         {
             Category = category;
             CategoryId = category.Id;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            Description = description;
+        }
+
+        public void DebitStock(int quantity)
+        {
+            if (quantity < 0) quantity *= -1;
+            StockQuantity -= quantity;
+        }
+
+        public void ReplenishStock(int quantity)
+        {
+            StockQuantity += quantity;
+        }
+
+        public bool HasStock(int quantity)
+        {
+            return StockQuantity >= quantity;
+        }
+
+        public void Validate()
+        {
         }
     }
 }
